653b080313a09677528409b660dbeaa00cc8bed3
NMMalika <94176433+NMMalika@users.noreply.github.com>
2025-01-07 20:59:41 +0300

    if conditions and for loops

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from datetime import datetime
 
 
 # Create your views here.
 def home(request):
+    context={
+        "name": ["John", "Doe", "Smith", "Jane"],
+        "can_display_name": True,
+    }
 
-    return render(request, "home.html", {})
+    return render(request, "home.html", context)
